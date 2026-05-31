411db4f22d583e2da26cc1c67ebbe4980056d8d8
Alhkcsj <fan343908@qq.com>
2026-05-31 20:12:38 +0800

    gzvm-stub: add missing gzvm_set_gic_bases() stub
    Prevents linker error in aarch64-softmmu builds without CONFIG_GZVM.

--- accel/stubs/gzvm-stub.c
+++ accel/stubs/gzvm-stub.c
@@ -8,6 +8,11 @@ int gzvm_arm_set_dtb(uint64_t dtb_start, uint64_t dtb_size)
     return -1;
 }
 
+void gzvm_set_gic_bases(uint64_t dist_base, uint64_t redist_base,
+                        uint64_t redist_size)
+{
+}
+
 void gzvm_set_ram_base(uint64_t base)
 {
 }
